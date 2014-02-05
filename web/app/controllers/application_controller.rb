f99007a011324ac2965c3a4d85c07f963546a889
Thomazr <me@thomaspockrandt.com>
2014-02-05 13:19:20 -0800

    Remove debug log

--- web/app/controllers/application_controller.rb
+++ web/app/controllers/application_controller.rb
@@ -8,6 +8,5 @@ class ApplicationController < ActionController::Base
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = "*"
     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-    puts "dkashdkjasdhaskdhkaj"
   end
 end
