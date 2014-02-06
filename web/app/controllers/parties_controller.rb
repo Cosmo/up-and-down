06f240539894a479b80b6a5ccc900e4fc294ee31
Thomazr <me@thomaspockrandt.com>
2014-02-06 14:45:40 -0800

    Only return movies

--- web/app/controllers/parties_controller.rb
+++ web/app/controllers/parties_controller.rb
@@ -17,34 +17,30 @@ class PartiesController < ApplicationController
     party = Party.where(vanity: params[:id]).first
     users = party.users.any? ? party.users : nil
 
-    movies = {}
-    party.users.each do |user|
-      user.answers.each do |answer|
-        movie = answer.movies.first
+    movies = Movie.all
 
-        if movies[movie.id]
-          movies[movie.id].upped += 1
-        else
-          movies[movie.id] = movie
-          movies[movie.id].upped = 1
-        end
-        
-        # if answer.upped
-        #   if movies[movie.id] == nil
-        #     movies[movie.id] = movie
-        #     movies[movie.id].upped = 1
-        #   else
-        #     movies[movie.id].upped += 1
-        #   end
-        # end
-      end
+    movies.each do |movie|
+      movie.upped = 1
     end
+
+    # movies = {}
+    # party.users.each do |user|
+    #   user.answers.each do |answer|
+    #     movie = answer.movies.first
+
+    #     movies[movie.id] = movie
+
+    #     if answer.upped
+    #       movies[movie.id].upped = (movies[movie.id].upped || 0) + 1
+    #     end
+    #   end
+    # end
 
     movies = movies.any? ? movies : nil
 
     # render :json => { users: users, movie.to_json(:methods => :upped) }
     # render json: movies.to_json(:methods => :upped)
 
-    render json: { users: users, movies: movies }
+    render json: movies, methods: :upped
   end
 end
