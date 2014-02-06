1a867b46a56aa724655067dc08ad0a035c444429
Thomazr <me@thomaspockrandt.com>
2014-02-06 15:10:09 -0800

    Return array

--- web/app/controllers/parties_controller.rb
+++ web/app/controllers/parties_controller.rb
@@ -17,32 +17,30 @@ class PartiesController < ApplicationController
     party = Party.where(vanity: params[:id]).first
     users = party.users.any? ? party.users : nil
 
-    grouped_movies = {}
-
     movies = Movie.all
-    movies.each do |movie|
-      grouped_movies[movie.id] = movie
-    end
+    # movies.each do |movie|
+    #   grouped_movies[movie.id] = movie
+    # end
 
     party.users.each do |user|
       user.answers.each do |answer|
-        movie = answer.movies.first
+        answer_movie = answer.movies.first
 
-        grouped_movies[movie.id] = movie
-
-        if answer.upped == nil
-          answer.upped = 1
-        else
-          answer.upped = answer.upped
+        if answer.upped
+          movies.each do |movie|
+            if movie.id == answer_movie.id
+              movie.upped += 1
+            end
+          end
         end
       end
     end
 
-    grouped_movies = grouped_movies.any? ? grouped_movies : nil
+    movies = movies.any? ? movies : nil
 
     # render :json => { users: users, movie.to_json(:methods => :upped) }
     # render json: movies.to_json(:methods => :upped)
 
-    render json: grouped_movies, methods: :upped
+    render json: movies, methods: :upped
   end
 end
