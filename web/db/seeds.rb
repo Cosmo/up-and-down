0a65a7faafcd05f08f8a8b21db01ac9ccb747596
olcaybuyan <olcay@buyan.de>
2014-02-06 13:48:24 -0800

    posters added

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -32,29 +32,45 @@ movie.questions << question6
 movie.questions << question7
 movie.questions << question8
 
-movie = Movie.create(name: "Revolutionary Road")
+movie = Movie.create(name: "Revolutionary Road", poster_url: "http://upload.wikimedia.org/wikipedia/en/2/28/Revolutionary_road.jpg")
 movie.questions << question1
 movie.questions << question5
 movie.questions << question6
 movie.questions << question8
 
-movie = Movie.create(name: "Malavita - The Family")
+movie = Movie.create(name: "Malavita - The Family", poster_url: "http://upload.wikimedia.org/wikipedia/en/7/75/The_Family_2013%2C_Poster.jpg")
 movie.questions << question4
 movie.questions << question10
 movie.questions << question9
 
-movie = Movie.create(name: "Shutter Island")
+movie = Movie.create(name: "Shutter Island", poster_url: "http://upload.wikimedia.org/wikipedia/en/7/76/Shutterislandposter.jpg")
 movie.questions << question1
 movie.questions << question4
 
-movie = Movie.create(name: "The Holiday")
+movie = Movie.create(name: "The Holiday", poster_url: "http://upload.wikimedia.org/wikipedia/en/6/60/Theholidayposter.jpg")
 movie.questions << question5
 movie.questions << question6
 movie.questions << question8
 movie.questions << question9
 
-movie = Movie.create(name: "All the King's Men")
+movie = Movie.create(name: "All the King's Men", poster_url: "http://upload.wikimedia.org/wikipedia/en/d/df/All_the_kings_men.jpg")
 movie.questions << question5
 movie.questions << question6
+movie.questions << question2
+
+movie = Movie.create(name: "Little Children", poster_url: "http://upload.wikimedia.org/wikipedia/en/8/81/Little_children_post.jpg")
+movie.questions << question5
+movie.questions << question8
+movie.questions << question6
+
+movie = Movie.create(name: "Naked in New York", poster_url: "http://upload.wikimedia.org/wikipedia/en/f/fa/Nakedinnewyork.jpg")
+movie.questions << question4
+movie.questions << question7
+movie.questions << question9
+
+movie = Movie.create(name: "Hugo Cabret", poster_url: "http://upload.wikimedia.org/wikipedia/en/7/73/Hugo_Poster.jpg")
+movie.questions << question4
 movie.questions << question8
 movie.questions << question9
+
+
