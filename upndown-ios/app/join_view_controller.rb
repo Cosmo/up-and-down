98ca6e60c843565dde9c03d1e5f4599d3dc82bda
Devran Ünal <devran@me.com>
2014-02-06 14:26:01 -0800

    Add hint

--- upndown-ios/app/join_view_controller.rb
+++ upndown-ios/app/join_view_controller.rb
@@ -1,7 +1,26 @@
 class JoinViewController < UIViewController
+  attr_accessor :hintLabel
+  
   def viewDidLoad
     super
     
-    self.view.backgroundColor = UIColor.blueColor
+    self.view.backgroundColor = UIColor.blackColor
+    
+    self.hintLabel = UILabel.alloc.initWithFrame(CGRectMake(0, 0, 0, 0)).tap do |label|
+      label.text                            = "Scan QR-Code"
+      label.font                            = UIFont.boldSystemFontOfSize(44)
+      label.numberOfLines                   = 0
+      label.adjustsLetterSpacingToFitWidth  = true
+      label.adjustsFontSizeToFitWidth       = true
+      label.textAlignment                   = NSTextAlignmentCenter
+      label.textColor                       = UIColor.whiteColor
+      self.view.addSubview(label)
+    end
+  end
+  
+  def viewWillLayoutSubviews
+    super
+    
+    self.hintLabel.frame  = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
   end
 end
